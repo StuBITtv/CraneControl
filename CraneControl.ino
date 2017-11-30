480b520bf5095f41e1ab9a523be95c193b7ec02f
StuBIT <haas.fabian@gmx.de>
2017-11-30 16:07:32 +0100

    Strange things happend :D

--- CraneControl.ino
+++ CraneControl.ino
@@ -1,14 +1,26 @@
+#include "AccelStepper.h"
+#define HALFSTEP 8
+
+#define motorPin1  12
+#define motorPin2  11
+#define motorPin3  10
+#define motorPin4  9
+
+AccelStepper stepper1(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
+
 void setup()
 {
-
+  stepper1.setMaxSpeed(1000.0);
+  stepper1.setSpeed(1000);
+  stepper1.moveTo(0);
 }
 
 void loop()
 {
-
+  
 }
 
-String getBluetoothString()
+String serialEvent()
 {
   /* Returns a optimized string of the HC-06 
    * Bluetooth device
